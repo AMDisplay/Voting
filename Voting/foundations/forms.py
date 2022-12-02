6dab0d533767e0b93cca780997a188fbe4b4d1da
Display <melnikov933@mail.ru>
2022-12-02 14:38:12 +0300

    validate forms

--- Voting/foundations/forms.py
+++ Voting/foundations/forms.py
@@ -2,13 +2,12 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from foundations.models import Voting, Found
 from users.models import User
-from django.core.validators import MaxLengthValidator
 
 
 class VotersRegisrationsForm(UserCreationForm):
     voting = forms.ModelChoiceField(queryset=Voting.objects.all())
     foundations = forms.ModelChoiceField(queryset=Found.objects.all())
-    id_card = MaxLengthValidator(limit_value=8)
+    id_card = forms.IntegerField(min_value=8, max_value=8)
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('id_card','first_name', 'last_name', 'voting', 'foundations', 'password1', 'password2')
